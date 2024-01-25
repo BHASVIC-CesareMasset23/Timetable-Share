8954a5173719d4ed187d6e4abcaf0b1af1558a43
Cesare M <cesare.masset23@students.bhasvic.ac.uk>
2024-01-25 17:21:19 +0000

    mini change

--- where_r_u.py
+++ where_r_u.py
@@ -1,7 +1,7 @@
-#where r u
+#where is everyone
 
 print("What day is it?\n[1] Monday\n[2] Tuesday\n[3] Wednesday\n[4] Thursday\n[5] Friday")
-day = int(input("Day: "))
+day = int(input("Option: "))
 hours = int(input("Hours: "))
 minutes = int(input("Minutes: "))
 #hours, minutes = input("Enter time: ").split()
